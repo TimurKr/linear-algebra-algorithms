d79a674ed3dd1cfd80d236a7e6bd5fb7c0c7f583
Timur Kramár <timurjan.kramar123@gmail.com>
2024-02-28 12:09:56 +0100

    Modularizing, now run from the terminal `python3 -m LU_decomposition.main

--- LU_decomposition/main.py
+++ LU_decomposition/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import numpy as np
 
 from utils.io import export_matrix, export_vector, read_matrix, read_vector
@@ -8,9 +9,9 @@ from utils.io import export_matrix, export_vector, read_matrix, read_vector
 def main():
     
     # Read the input matrix "matica.txt"
-    matrix = read_matrix("inputs/matrix.txt")
+    matrix = read_matrix("inputs/matrix.txt", os.path.dirname(__file__))
     # Read the input vector "ps.txt"
-    vector = read_vector("inputs/vector.txt")
+    vector = read_vector("inputs/vector.txt", os.path.dirname(__file__))
 
     I = np.eye(len(matrix))
 
@@ -20,9 +21,9 @@ def main():
             matrix[j][k:] -= I[j][k] * matrix[k][k:]
 
     # Export the matrix "matica.txt" to "matica_out.txt"
-    export_matrix(matrix, "outputs/matrix.txt")
+    export_matrix(matrix, "outputs/matrix.txt", os.path.dirname(__file__))
     # Export the vector "ps.txt" to "ps_out.txt"
-    export_vector(vector, "outputs/vector.txt")
+    export_vector(vector, "outputs/vector.txt", os.path.dirname(__file__))
 
     return
 
--- utils/io.py
+++ utils/io.py
@@ -2,23 +2,23 @@ import os
 import numpy as np
 
 
-def read_matrix(filePath: str) -> np.ndarray:
-    with open(os.path.join(os.path.dirname(__file__), filePath), "r") as file:
-        matrix = [[float(e) for e in row.split() if e] for row in file]
-    return np.array(matrix)
+def read_matrix(filePath: str, dir: str) -> np.ndarray:
+  with open(os.path.join(dir, filePath), "r") as file:
+    matrix = [[float(e) for e in row.split() if e] for row in file]
+  return np.array(matrix, dtype=float)
 
-def read_vector(filePath: str) -> np.ndarray:
-    with open(os.path.join(os.path.dirname(__file__), filePath), "r") as file:
+def read_vector(filePath: str, dir: str) -> np.ndarray:
+    with open(os.path.join(dir, filePath), "r") as file:
         vector = [float(e) for e in file.read().split() if e]
     return np.array(vector)
 
-def export_matrix(matrix: np.ndarray, filePath: str) -> None:
-    with open(os.path.join(os.path.dirname(__file__), filePath), "w") as file:
+def export_matrix(matrix: np.ndarray, filePath: str, dir: str) -> None:
+    with open(os.path.join(dir, filePath), "w") as file:
         for row in matrix:
             file.write(" ".join([str(e)[:11].ljust(12) for e in row]) + "\n")
     return
 
-def export_vector(vector: np.ndarray, filePath: str) -> None:
-    with open(os.path.join(os.path.dirname(__file__), filePath), "w") as file:
+def export_vector(vector: np.ndarray, filePath: str, dir: str) -> None:
+    with open(os.path.join(dir, filePath), "w") as file:
         file.write("\n".join([str(v)[:8].ljust(8) for v in vector]))
     return 
